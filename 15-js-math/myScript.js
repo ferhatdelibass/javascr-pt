9514f7191b72db181c39214ea71701a4b3c4baa1
Ferhat <ffdelibas@gmail.com>
2023-03-20 22:44:49 +0300

    devam

---
+++ 15-js-math/myScript.js
@@ -0,0 +1,17 @@
+console.log(Math.round(4.3))
+//4.5 den yukarı 5, aşağısı için 4 yapar
+console.log(Math.ceil(4.3))
+//üste yuvarlar
+console.log(Math.floor(4.9))
+//aşağı yuvarlar
+console.log(Math.trunc(4.24))
+//tam syı kısmını verir
+console.log(Math.pow(8,3))
+//üs alma
+console.log(Math.sqrt(64))
+//kare kökünü verir
+console.log(Math.min(0,150,39,47,55,1,-4))
+// yazılan minimum sayıyıy verir
+console.log(Math.max(0,150,39,47,55,1,-4))
+//max sayıyı verir
+console.log(Math.random())
