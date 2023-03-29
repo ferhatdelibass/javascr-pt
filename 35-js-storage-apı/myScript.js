b0f1375b033262760428b86f56c8d1e03e68d05e
Ferhat <ffdelibas@gmail.com>
2023-03-29 23:16:18 +0300

    devam

--- 35-js-storage-apı/myScript.js
+++ 35-js-storage-apı/myScript.js
@@ -13,3 +13,19 @@
 
 //soru: üzerine yazmayıp ekleme yapması için ne yapabiliriz?
 //biz sadece name adlı bir veri olduğunu biliyoruz.
+
+//
+
+// const name1 = ["Jhon Doe","Lee Doe"]
+// localStorage.setItem("name",Json.stringify(name1));
+// const item = JSON.parse(localStorage.getItem("name"))
+// console.log(item)
+// const name2 = [...JSON.parse(localStorage.getItem("name")),"Jhon Ray","Lee Ray"]
+// localStorage.setItem("name",JSON.stringify(name2));
+// const item2 = JSON.parse(localStorage.getItem("name"))
+// console.log(item2)
+
+// localStorage.removeItem("name");
+// localStorage.clear();
+
+// sessionStorage.setItem("name","Ahmet","Berk")
